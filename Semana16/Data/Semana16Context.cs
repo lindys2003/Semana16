517660496f3eeac2ee16768509fa2819aac33a1f
Lindys Arely Martinez Herrera <herrerarely2003@gmail.com>
2022-12-04 20:50:29 -0600

    Agregar cambios

---
+++ Semana16/Data/Semana16Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using semana16_1.Controllers;
+using semana16_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace semana16.Data
+{
+    public class semana16Context : DbContext
+    {
+        public semana16Context(DbContextOptions<semana16Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+        public static implicit operator semana16Context(semana16_1Context v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Semana16/Startup.cs
+++ Semana16/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Semana16
+namespace semana16_1
 {
     public class Startup
     {
@@ -24,6 +20,9 @@ namespace Semana16
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<semana16.Data.semana16Context>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("semana16-1Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,3 +54,5 @@ namespace Semana16
         }
     }
 }
+
+    
